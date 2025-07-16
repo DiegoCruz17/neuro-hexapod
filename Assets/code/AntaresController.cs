34c99e83b29ce3a3dbb8d8f745918460128f925f
DiegoCruz17 <degocruzruiz@gmail.com>
2025-07-15 21:19:17 -0500

    controller update

--- Assets/code/AntaresController.cs
+++ Assets/code/AntaresController.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class AntaresController : MonoBehaviour
 {
+    public bool disableGravity = false;
+    
     public Transform Cooxa1;
     public Transform Cooxa2;
     public Transform Cooxa3;
@@ -17,15 +19,7 @@ public class AntaresController : MonoBehaviour
 
     public float d = 40f, al = 60f, n = 20f, w = 1f, rs = 0f, ra = 0f, c = 0f, k = 0f;
 
-    private Vector3[] mountPoints = new Vector3[]
-    {
-        new Vector3(62.77f,  90.45f, 123.83f),
-        new Vector3(86f,     0f,     123.83f),
-        new Vector3(65.89f, -88.21f, 123.83f),
-        new Vector3(-65.89f, 88.21f, 123.83f),
-        new Vector3(-86f,    0f,     123.83f),
-        new Vector3(-62.77f, -90.45f, 123.83f)
-    };
+    private Vector3[] mountPoints;
 
     private Transform[] coxas;
     private Transform[] femurs;
@@ -33,6 +27,31 @@ public class AntaresController : MonoBehaviour
 
     void Start()
     {
+        if (disableGravity)
+        {
+            // Disable gravity on this object
+            var rootBody = GetComponent<ArticulationBody>();
+            if (rootBody != null)
+                rootBody.useGravity = false;
+
+            // Disable gravity on all children and grandchildren
+            foreach (var body in GetComponentsInChildren<ArticulationBody>())
+            {
+                body.useGravity = false;
+            }
+        }
+
+        // Initialize mount points with current transform position
+        mountPoints = new Vector3[]
+        {
+            new Vector3(62.77f,  90.45f, transform.position.y),
+            new Vector3(86f,     0f,     transform.position.y),
+            new Vector3(65.89f, -88.21f, transform.position.y),
+            new Vector3(-65.89f, 88.21f, transform.position.y),
+            new Vector3(-86f,    0f,     transform.position.y),
+            new Vector3(-62.77f, -90.45f, transform.position.y)
+        };
+
         coxas = new Transform[] { Cooxa1, Cooxa2, Cooxa3, Cooxa4, Cooxa5, Cooxa6 };
         femurs = new Transform[6];
         tibias = new Transform[6];
@@ -42,22 +61,42 @@ public class AntaresController : MonoBehaviour
             tibias[i] = femurs[i].GetChild(0);
         }
     }
-
     void Update()
     {
+
         var targets = HexapodTrajectory.CalcularTrayectoria(d, al, n, w, rs, ra, c, k);
 
         for (int i = 0; i < 6; i++)
         {
+            var angleModifier = 1f;
+            if (i < 3)
+            {
+                angleModifier = -1f;
+            }
             Vector3 basePos = mountPoints[i];
             Vector3 target = targets[i];
             Vector3 angles = HexapodKinematics.InverseKinematics(basePos, target, L0, L1, L2);
+            Debug.Log("Angles: " + angles);
 
-            // Aplicar ángulos: θ1 a coxa (Y), θ2 a femur (Z), θ3 a tibia (Z)
-            // Ajustar ejes según sea necesario para coincidir con la orientación de tu modelo
-            coxas[i].localRotation = Quaternion.Euler(0, angles.x, 0);
-            femurs[i].localRotation = Quaternion.Euler(0, 0, angles.y);
-            tibias[i].localRotation = Quaternion.Euler(0, 0, angles.z);
+            //cooxa
+            var coxaBody = coxas[i].GetComponent<ArticulationBody>();
+            var coxaDrive = coxaBody.xDrive;
+            coxaDrive.target = angles.x * angleModifier;
+            coxaBody.xDrive = coxaDrive;
+            // Debug.Log("Coxa " + i + " target: " + angles.x * angleModifier);
+
+            //femur
+            var femurBody = femurs[i].GetComponent<ArticulationBody>();
+            var femurDrive = femurBody.xDrive;
+            femurDrive.target = angles.y * angleModifier;
+            femurBody.xDrive = femurDrive;
+            // Debug.Log("Femur " + i + " target: " + angles.y * angleModifier);
+            //tibia 
+            var tibiaBody = tibias[i].GetComponent<ArticulationBody>();
+            var tibiaDrive = tibiaBody.xDrive;
+            tibiaDrive.target = angles.z * angleModifier;
+            tibiaBody.xDrive = tibiaDrive;
+            // Debug.Log("Tibia " + i + " target: " + angles.z * angleModifier);
         }
     }
 }
